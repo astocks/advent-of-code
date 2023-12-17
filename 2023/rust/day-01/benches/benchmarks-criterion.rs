9b38849b59b7f0ad4592ce15b9a3c9b3eddbf945
Adam Stocks <adam@adamstocks.com>
2023-12-17 10:39:29 -0500

    Add  files and implement game parsing logic function

--- 2023/rust/day-01/benches/benchmarks-criterion.rs
+++ 2023/rust/day-01/benches/benchmarks-criterion.rs
@@ -21,18 +21,6 @@ fn criterion_benchmark_part2(c: &mut Criterion) {
     group.bench_with_input("part2", input, |b, input| {
         b.iter(|| part2::process(input))
     });
-    group.bench_with_input(
-        "part2_nom",
-        input,
-        |b, input| b.iter(|| part2_nom::process(input)),
-    );
-    group.bench_with_input(
-        "part2_aho_corasick",
-        input,
-        |b, input| {
-            b.iter(|| part2_aho_corasick::process(input))
-        },
-    );
     group.finish();
 }
 
--- 2023/rust/day-02/src/part1.rs
+++ 2023/rust/day-02/src/part1.rs
@@ -1,8 +1,6 @@
-
 use crate::custom_error::AocError;
 
-
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Color {
     Red,
     Blue,
@@ -22,45 +20,72 @@ struct Game {
     rounds: Vec<Round>,
 }
 
+impl Game {
+    fn find_valid_games(
+        &self,
+        sample_game: &Game,
+    ) -> Vec<Game> {
+        todo!("find valid games")
+    }
+}
+
+struct RGB(u32, u32, u32);
 
 #[tracing::instrument]
 pub fn process(
     input: &str,
 ) -> miette::Result<String, AocError> {
-   //this function should split the input into lines, then split on :, and return the parts. 
-        let lines = input.lines();
-        for line in lines {
-            let (game_num, value) = line.split_once(":").unwrap();
-            let game_num = game_num.trim().split_once(" ").unwrap().1;
-            let value = value.trim();
-            let mut game = Game {
-                game_number: game_num.parse::<u32>().unwrap(),
-                rounds: Vec::new(),
-            };
-            for round in value.split(";") {
-                let mut r = Round {
-                    cubes: Vec::new(),
-                };
-                for count_of_color in round.split(",") {
-                    let count = count_of_color.trim().split_once(" ").unwrap().0.parse::<u32>().unwrap();
-                    let color = count_of_color.trim().split_once(" ").unwrap().1;
-                    let cube = match color.to_lowercase().as_str() {
-                        "red" => Cube(Color::Red, count),
-                        "blue" => Cube(Color::Blue, count),
-                        "green" => Cube(Color::Green, count),
-                        _ => panic!("unable to match {color}"),
-                    };
-                    r.cubes.push(cube);
-                }
-                game.rounds.push(r);
-            } 
-            
-            dbg!(game);
-        }
-        
-    Ok("make it pass".to_string())
+    parse_games(input); // take file with game data and parse it into a
+                        // vector of games
 
+    Ok("this still isn't finished".to_string())
+}
+
+fn parse_games(input: &str) -> Vec<Game> {
+    let lines = input.lines();
+    let mut games: Vec<Game> = Vec::new();
+    for line in lines {
+        let (game_num, value) =
+            line.split_once(":").unwrap();
+        let game_num =
+            game_num.trim().split_once(" ").unwrap().1;
+        let value = value.trim();
+        let mut game = Game {
+            game_number: game_num.parse::<u32>().unwrap(),
+            rounds: Vec::new(),
+        };
+        for round in value.split(";") {
+            let mut r = Round { cubes: Vec::new() };
+            for count_of_color in round.split(",") {
+                let count = count_of_color
+                    .trim()
+                    .split_once(" ")
+                    .unwrap()
+                    .0
+                    .parse::<u32>()
+                    .unwrap();
+                let color = count_of_color
+                    .trim()
+                    .split_once(" ")
+                    .unwrap()
+                    .1;
+                let cube = match color
+                    .to_lowercase()
+                    .as_str()
+                {
+                    "red" => Cube(Color::Red, count),
+                    "blue" => Cube(Color::Blue, count),
+                    "green" => Cube(Color::Green, count),
+                    _ => panic!("unable to match {color}"),
+                };
+                r.cubes.push(cube);
+            }
+            game.rounds.push(r);
+        }
+        games.push(game)
     }
+    games
+}
 
 #[cfg(test)]
 mod tests {
@@ -73,7 +98,7 @@ mod tests {
         Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
         Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
         Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
-        assert_eq!("", process(input)?);
+        assert_eq!("8", process(input)?);
         Ok(())
     }
 }
